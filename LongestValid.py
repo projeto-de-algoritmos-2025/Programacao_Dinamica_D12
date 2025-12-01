989da4f6b369c4fe00d4f760d22add295a20d027
JuliaSSouza <juliasantanna.souza@gmail.com>
2025-12-01 18:51:22 -0300

    ajustes
    Co-authored-by: Carolina Barbosa <carolinabarbosab09@gmail.com>

--- LongestValid.py
+++ LongestValid.py
@@ -1,19 +1,24 @@
 class Solution(object):
     def longestValidParentheses(self, s):
-
-        stack = [-1]
         max_len = 0
+        dp = [0] * len(s)
         
-        for i, char in enumerate(s):
-            if char == '(':
-                stack.append(i)
-            else:
-                stack.pop()
+        for i in range(1, len(s)):
+            if s[i] == ')':
                 
-                if not stack:
-                    stack.append(i)
-                else:
-                    max_len = max(max_len, i - stack[-1])
+                if s[i - 1] == '(':
+                    if i >= 2:
+                        dp[i] = dp[i - 2] + 2
+                    else:
+                        dp[i] = 2
+                
+                elif i - dp[i - 1] > 0 and s[i - dp[i - 1] - 1] == '(':
+
+                    prev_len = dp[i - 1] 
+                    before_match = dp[i - dp[i - 1] - 2] if (i - dp[i - 1]) >= 2 else 0
                     
+                    dp[i] = prev_len + 2 + before_match
+            
+            max_len = max(max_len, dp[i])
+            
         return max_len
-        
